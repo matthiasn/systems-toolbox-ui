7a6b5bf2399800129e730b6946489b64cd742cd5
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2016-02-24 22:00:29 +0100

    v0.5.4: latest React version

--- changes.md
+++ changes.md
@@ -1,3 +1,30 @@
+## v0.5.4 - February 24th, 2016
+
+```clojure
+[matthiasn/systems-toolbox-ui "0.5.4"]
+```
+
+* latest React version
+
+
+## v0.5.3 - February 9th, 2016
+
+```clojure
+[matthiasn/systems-toolbox-ui "0.5.3"]
+```
+
+* latest React version
+
+
+## v0.5.2 - January 9th, 2016
+
+```clojure
+[matthiasn/systems-toolbox-ui "0.5.2"]
+```
+
+* latest dependencies
+
+
 ## v0.5.1 - January 3rd, 2016
 
 ```clojure
@@ -5,4 +32,3 @@
 ```
 
 * UI components moved from systems-toolbox into separate repo
-
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject matthiasn/systems-toolbox-ui "0.5.3"
+(defproject matthiasn/systems-toolbox-ui "0.5.4"
   :description "UI components for systems-toolbox"
   :url "https://github.com/matthiasn/systems-toolbox"
   :license {:name "Eclipse Public License"
@@ -9,7 +9,7 @@
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/clojurescript "1.7.228"]
                  [reagent "0.5.1" :exclusions [cljsjs/react]]
-                 [cljsjs/react-with-addons "0.13.3-0"]]
+                 [cljsjs/react "0.14.3-0"]]
 
   :plugins [[lein-codox "0.9.4" :exclusions [org.clojure/clojure]]
             [lein-cljsbuild "1.1.2"]])
