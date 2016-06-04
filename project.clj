3e19caa0533581cba9689aca1bb050f6c462dd97
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2016-06-04 12:05:19 +0200

    v0.5.9: dependencies; no dependency on specific Clojure version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject matthiasn/systems-toolbox-ui "0.5.8"
+(defproject matthiasn/systems-toolbox-ui "0.5.9"
   :description "UI components for systems-toolbox"
   :url "https://github.com/matthiasn/systems-toolbox"
   :license {:name "Eclipse Public License"
@@ -6,9 +6,10 @@
 
   :source-paths ["src/clj" "src/cljs" "src/cljc"]
 
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/clojurescript "1.8.51"]
-                 [reagent "0.6.0-alpha"]]
+  :dependencies [[reagent "0.6.0-alpha"]]
+
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.9.0-alpha3"]
+                                  [org.clojure/clojurescript "1.9.36"]]}}
 
   :plugins [[lein-codox "0.9.5" :exclusions [org.clojure/clojure]]
             [lein-cljsbuild "1.1.3"]])
